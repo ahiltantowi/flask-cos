711e9ab25559de72f10d4d7bed9ccc617e12e2be
Hilman Tantowi <ahiltantowi@gmail.com>
2026-01-02 08:29:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
-
 @app.route("/")
-def home():
-    return "Flask COS jalan 🚀"
+def index():
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run()
+if __name__=="__main__":
+    app.run(debug=True)
